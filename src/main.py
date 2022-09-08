adbe298acbfa331967f3553ddefd63ee46dacf8e
Lucas R <github@lucas.rs>
2022-09-08 09:58:27 -0300

    typing added, fix formatting

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import re
 from dataclasses import dataclass
 from socket import AF_INET, SHUT_WR, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
-from typing import Dict
+from typing import Callable, NoReturn
 
 
 @dataclass
 class Request:
     method: str
-    headers: Dict[str, str]
+    headers: dict[str, str]
     path: str
     body: str
 
@@ -19,23 +19,26 @@ class Response:
     content_type: str = "text/html"
 
 
-class PicoFlask:
-    routes = []
+View = Callable[[Request], Response]
 
-    def route(self, path):
-        def decorator(func):
+
+class PicoFlask:
+    routes: list[tuple[re.Pattern, View]] = []
+
+    def route(self, path: str) -> Callable[[View], View]:
+        def decorator(func: View) -> View:
             self.routes.append((re.compile(path + "$"), func))
             return func
 
         return decorator
 
-    def get_route(self, request_path):
+    def get_route(self, req_path: str) -> View:
         for path, route_func in self.routes:
-            if path.match(request_path):
+            if path.match(req_path):
                 return route_func
         raise Exception("no matching path")
 
-    def parse_request(self, request):
+    def parse_request(self, request: str) -> Request:
         first_line, request = request.split("\r\n", maxsplit=1)
         method, path, _ = first_line.split(" ")
         headers_str, body = request.split("\r\n\r\n")
@@ -45,50 +48,41 @@ class PicoFlask:
             headers[header] = value
         return Request(method, headers, path, body)
 
-    def build_response(self, request):
-        return f"""HTTP/1.1 {request.status_code}\r
-        Content-Type: {request.content_type}; chartset=utf-8\r
-        Content-Length: {len(request.body)}\r
-        Connection: close\r\n\r
-        {request.body}"""
+    def build_response(self, response: Response) -> str:
+        return (
+            f"HTTP/1.1 {response.status_code}\r\n"
+            f"Content-Type: {response.content_type}; chartset=utf-8\r\n"
+            f"Content-Length: {len(response.body)}\r\n"
+            f"Connection: close\r\n\r\n"
+            f"{response.body}"
+        )
 
-    def run(self, addr, port):
+    def run(self, addr: str = "0.0.0.0", port: int = 8000) -> NoReturn:
+        print(f"Running on {addr}:{port}")
         with socket(AF_INET, SOCK_STREAM) as s:
             s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
             s.bind((addr, port))
             s.listen(5)
             while True:
                 client, _ = s.accept()
-                request = client.recv(4096).decode()
+                raw_req = client.recv(4096).decode()
                 try:
-                    parsed_req = self.parse_request(request)
-                    print("[REQUEST]\t", parsed_req.method, parsed_req.path)
-                    route = self.get_route(parsed_req.path)
-                    response = route(parsed_req)
-                except:
-                    response = Response("server error", 500)
-                print("[RESPONSE]\t", response.status_code, response.body)
-                client.sendall(self.build_response(response).encode())
+                    req = self.parse_request(raw_req)
+                    print(f"[REQUEST]\t {req.method} '{req.path}'")
+                    route = self.get_route(req.path)
+                    resp = route(req)
+                except Exception as e:
+                    resp = Response(f"server error: {e}", 500)
+                print(f"[RESPONSE]\t {resp.status_code} {resp.body}")
+                client.sendall(self.build_response(resp).encode())
                 client.shutdown(SHUT_WR)
 
 
 if __name__ == "__main__":
     app = PicoFlask()
 
-    @app.route("/page1")
-    def page1(request):
-        return Response("you're on page 1")
-
-    @app.route("/page2")
-    def page1(request):
-        return Response("you're on page 2")
-
-    @app.route("/test")
-    def test(request):
-        return Response("cool framework")
-
     @app.route("/")
-    def test(request):
-        return Response("main")
+    def index(request: Request) -> Response:
+        return Response("<b>It works</b>")
 
     app.run("0.0.0.0", 8000)
